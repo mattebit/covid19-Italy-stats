774257fa5fcb1139511d425f79fac678c94c259e
mattebit <matteobit@hotmail.com>
2020-11-14 18:22:00 +0100

    upgraded graph

--- js/graphs.js
+++ js/graphs.js
@@ -13,7 +13,6 @@ const fillGraphs = async () => {
         
         // Create chart instance
         var c_positivi_tamponi = am4core.create("positivi_tamponi", am4charts.XYChart);
-        c_positivi_tamponi.data = j_r_positivi_tamponi;
         
         // Create axes
         var dateAxis = c_positivi_tamponi.xAxes.push(new am4charts.DateAxis());
@@ -21,10 +20,35 @@ const fillGraphs = async () => {
         
         var valueAxis = c_positivi_tamponi.yAxes.push(new am4charts.ValueAxis());
         
+        /* Create series */
+        var columnSeries = c_positivi_tamponi.series.push(new am4charts.ColumnSeries());
+        columnSeries.name = "positives";
+        columnSeries.dataFields.valueY = "positives";
+        columnSeries.dataFields.dateX = "date";
+
+        //columnSeries.columns.template.tooltipText = "[#fff font-size: 15px]{name} in {categoryX}:\n[/][#fff font-size: 20px]{valueY}[/] [#fff]{additional}[/]"
+        //columnSeries.columns.template.propertyFields.fillOpacity = "fillOpacity";
+        //columnSeries.columns.template.propertyFields.stroke = "stroke";
+        //columnSeries.columns.template.propertyFields.strokeWidth = "strokeWidth";
+        //columnSeries.columns.template.propertyFields.strokeDasharray = "columnDash";
+        columnSeries.columns.template.strokeOpacity = 0;
+        
+        var rValueAxis = c_positivi_tamponi.yAxes.push(new am4charts.ValueAxis());
+        rValueAxis.renderer.opposite = true;
+        //rValueAxis.min = 0;
+        //rValueAxis.max = 100;
+        rValueAxis.strictMinMax = true;
+        rValueAxis.renderer.grid.template.disabled = true;
+        rValueAxis.numberFormatter = new am4core.NumberFormatter();
+        rValueAxis.numberFormatter.numberFormat = "#'%'"
+        rValueAxis.cursorTooltipEnabled = false;
+
         // Create series
         var series = c_positivi_tamponi.series.push(new am4charts.LineSeries());
-        series.dataFields.valueY = "value";
+        series.name = "r"
+        series.dataFields.valueY = "r";
         series.dataFields.dateX = "date";
+        series.yAxis = rValueAxis
         series.strokeWidth = 2;
         series.minBulletDistance = 10;
         series.tooltipText = "{valueY}";
@@ -33,14 +57,19 @@ const fillGraphs = async () => {
         series.tooltip.background.fillOpacity = 0.5;
         series.tooltip.label.padding(12,12,12,12)
         
+        
         // Add scrollbar
         c_positivi_tamponi.scrollbarX = new am4charts.XYChartScrollbar();
-        c_positivi_tamponi.scrollbarX.series.push(series);
+        c_positivi_tamponi.scrollbarX.series.push(columnSeries);
         
         // Add cursor
         c_positivi_tamponi.cursor = new am4charts.XYCursor();
-        c_positivi_tamponi.cursor.xAxis = dateAxis;
-        c_positivi_tamponi.cursor.snapToSeries = series;
+        c_positivi_tamponi.cursor = new am4charts.XYCursor();
+        c_positivi_tamponi.cursor.behavior = "panX";
+        //c_positivi_tamponi.cursor.xAxis = dateAxis;
+        //c_positivi_tamponi.cursor.snapToSeries = series;
+
+        c_positivi_tamponi.data = j_r_positivi_tamponi;
 
         var c_ricoverati_positivi = am4core.create("ricoverati_positivi", am4charts.XYChart);
         c_ricoverati_positivi.data = j_r_ricoverati_positivi;
