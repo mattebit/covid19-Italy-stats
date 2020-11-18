8779d4ad0ac505ba90f977347fdaa5011da311ba
mattebit <matteobit@hotmail.com>
2020-11-18 15:32:08 +0100

    New index

--- js/graphs.js
+++ js/graphs.js
@@ -1,8 +1,52 @@
+(function ($) {
+
+    "use strict";
+
+    var fullHeight = function () {
+
+        $('.js-fullheight').css('height', $(window).height());
+        $(window).resize(function () {
+            $('.js-fullheight').css('height', $(window).height());
+        });
+
+    };
+    fullHeight();
+})(jQuery);
+
+document.getElementById("sidebarCollapse").onclick = () => {
+
+    var sidebar = document.getElementById("sidebar");
+    
+    function updategraphs() {
+        c_positivi_tamponi.svgContainer.measure();
+        c_ricoverati_positivi.svgContainer.measure();
+        c_terapia_ospedalizzati.svgContainer.measure();
+        c_decessi_20_decessi_15_18.svgContainer.measure();
+        c_decessi_covid_decessi_20.svgContainer.measure();
+    }
+    // Code for Chrome, Safari and Opera
+    sidebar.addEventListener("webkitTransitionEnd", updategraphs);
+
+    // Standard syntax
+    sidebar.addEventListener("transitionend", updategraphs); 
+
+    sidebar.classList.toggle('active');
+}
+
+document.getElementById("sidebar").classList.toggle('active');
+
+// Create chart instance
+var c_positivi_tamponi = undefined
+var c_ricoverati_positivi = undefined
+var c_terapia_ospedalizzati = undefined
+var c_decessi_20_decessi_15_18 = undefined
+var c_decessi_covid_decessi_20 = undefined
+
 const fillGraphs = async () => {
     // Fetchs all jsons
     fetch("js/datas/j_r_positivi_tamponi.json")
-    .then(response => response.json())
-    .then(data => plot_c_positivi_tamponi(data))
+        .then(response => response.json())
+        .then(data => plot_c_positivi_tamponi(data))
 
     fetch("js/datas/j_r_ricoverati_positivi.json")
         .then(response => response.json())
@@ -12,6 +56,14 @@ const fillGraphs = async () => {
         .then(response => response.json())
         .then(data => plot_c_terapia_ospedalizzati(data));
 
+    fetch("js/datas/j_r_decessi_20_decessi_15_18.json")
+        .then(response => response.json())
+        .then(data => plot_c_decessi_20_decessi_15_18(data))
+
+    fetch("js/datas/j_r_decessi_covid_decessi_20.json")
+        .then(response => response.json())
+        .then(data => plot_c_decessi_covid_decessi_20(data))
+
     am4core.ready(function () {
         am4core.useTheme(am4themes_dataviz);
         //am4core.useTheme(am4themes_animated);
@@ -20,8 +72,7 @@ const fillGraphs = async () => {
 }
 
 function plot_c_positivi_tamponi(data) {
-    // Create chart instance
-    var c_positivi_tamponi = am4core.create("positivi_tamponi", am4charts.XYChart);
+    c_positivi_tamponi = am4core.create("positivi_tamponi", am4charts.XYChart);
 
     // Create axes
     var dateAxis = c_positivi_tamponi.xAxes.push(new am4charts.DateAxis());
@@ -35,12 +86,13 @@ function plot_c_positivi_tamponi(data) {
     columnSeries.dataFields.valueY = "positives";
     columnSeries.dataFields.dateX = "date";
 
+
     //columnSeries.columns.template.tooltipText = "[#fff font-size: 15px]{name} in {categoryX}:\n[/][#fff font-size: 20px]{valueY}[/] [#fff]{additional}[/]"
     //columnSeries.columns.template.propertyFields.fillOpacity = "fillOpacity";
     //columnSeries.columns.template.propertyFields.stroke = "stroke";
     //columnSeries.columns.template.propertyFields.strokeWidth = "strokeWidth";
     //columnSeries.columns.template.propertyFields.strokeDasharray = "columnDash";
-    columnSeries.columns.template.strokeOpacity = 0;
+    columnSeries.columns.template.strokeOpacity = 1;
 
     var rValueAxis = c_positivi_tamponi.yAxes.push(new am4charts.ValueAxis());
     rValueAxis.renderer.opposite = true;
@@ -63,7 +115,7 @@ function plot_c_positivi_tamponi(data) {
     series.tooltipText = "{valueY}";
     series.tooltip.pointerOrientation = "vertical";
     series.tooltip.background.cornerRadius = 20;
-    series.tooltip.background.fillOpacity = 0.5;
+    series.tooltip.background.fillOpacity = 1;
     series.tooltip.label.padding(12, 12, 12, 12)
 
 
@@ -77,12 +129,12 @@ function plot_c_positivi_tamponi(data) {
     c_positivi_tamponi.cursor.snapToSeries = series;
 
     c_positivi_tamponi.legend = new am4charts.Legend();
-
+    c_positivi_tamponi.svgContainer.autoResize = false;
     c_positivi_tamponi.data = data;
 }
 
 function plot_c_ricoverati_positivi(data) {
-    var c_ricoverati_positivi = am4core.create("ricoverati_positivi", am4charts.XYChart);
+    c_ricoverati_positivi = am4core.create("ricoverati_positivi", am4charts.XYChart);
     c_ricoverati_positivi.data = data;
 
     // Create axes
@@ -101,7 +153,7 @@ function plot_c_ricoverati_positivi(data) {
     series.numberFormatter.numberFormat = "#'%'"
     series.tooltip.pointerOrientation = "vertical";
     series.tooltip.background.cornerRadius = 20;
-    series.tooltip.background.fillOpacity = 0.5;
+    series.tooltip.background.fillOpacity = 1;
     series.tooltip.label.padding(12, 12, 12, 12)
 
     // Add scrollbar
@@ -112,10 +164,11 @@ function plot_c_ricoverati_positivi(data) {
     c_ricoverati_positivi.cursor = new am4charts.XYCursor();
     c_ricoverati_positivi.cursor.xAxis = dateAxis;
     c_ricoverati_positivi.cursor.snapToSeries = series;
+    c_ricoverati_positivi.svgContainer.autoResize = false;
 }
 
 function plot_c_terapia_ospedalizzati(data) {
-    var c_terapia_ospedalizzati = am4core.create("c_terapia_ospedalizzati", am4charts.XYChart);
+    c_terapia_ospedalizzati = am4core.create("c_terapia_ospedalizzati", am4charts.XYChart);
     c_terapia_ospedalizzati.data = data;
 
     // Create axes
@@ -133,7 +186,7 @@ function plot_c_terapia_ospedalizzati(data) {
     series.tooltipText = "{valueY}";
     series.tooltip.pointerOrientation = "vertical";
     series.tooltip.background.cornerRadius = 20;
-    series.tooltip.background.fillOpacity = 0.5;
+    series.tooltip.background.fillOpacity = 1;
     series.tooltip.label.padding(12, 12, 12, 12)
     series.numberFormatter.numberFormat = "#'%'"
 
@@ -145,9 +198,75 @@ function plot_c_terapia_ospedalizzati(data) {
     c_terapia_ospedalizzati.cursor = new am4charts.XYCursor();
     c_terapia_ospedalizzati.cursor.xAxis = dateAxis;
     c_terapia_ospedalizzati.cursor.snapToSeries = series;
-
+    c_terapia_ospedalizzati.svgContainer.autoResize = false;
 }
 
+function plot_c_decessi_20_decessi_15_18(data) {
+    c_decessi_20_decessi_15_18 = am4core.create("c_decessi_20_decessi_15_18", am4charts.XYChart);
+    c_decessi_20_decessi_15_18.data = data;
 
+    // Create axes
+    var dateAxis = c_decessi_20_decessi_15_18.xAxes.push(new am4charts.DateAxis());
+    dateAxis.renderer.minGridDistance = 50;
+
+    var valueAxis = c_decessi_20_decessi_15_18.yAxes.push(new am4charts.ValueAxis());
+
+    // Create series
+    var series = c_decessi_20_decessi_15_18.series.push(new am4charts.LineSeries());
+    series.dataFields.valueY = "value";
+    series.dataFields.dateX = "date";
+    series.strokeWidth = 2;
+    series.minBulletDistance = 10;
+    series.tooltipText = "{valueY}";
+    series.tooltip.pointerOrientation = "vertical";
+    series.tooltip.background.cornerRadius = 20;
+    series.tooltip.background.fillOpacity = 1;
+    series.tooltip.label.padding(12, 12, 12, 12)
+    series.numberFormatter.numberFormat = "#'%'"
+
+    // Add scrollbar
+    c_decessi_20_decessi_15_18.scrollbarX = new am4charts.XYChartScrollbar();
+    c_decessi_20_decessi_15_18.scrollbarX.series.push(series);
+
+    // Add cursor
+    c_decessi_20_decessi_15_18.cursor = new am4charts.XYCursor();
+    c_decessi_20_decessi_15_18.cursor.xAxis = dateAxis;
+    c_decessi_20_decessi_15_18.cursor.snapToSeries = series;
+    c_decessi_20_decessi_15_18.svgContainer.autoResize = false;
+}
+
+function plot_c_decessi_covid_decessi_20(data) {
+    c_decessi_covid_decessi_20 = am4core.create("c_decessi_covid_decessi_20", am4charts.XYChart);
+    c_decessi_covid_decessi_20.data = data;
+
+    // Create axes
+    var dateAxis = c_decessi_covid_decessi_20.xAxes.push(new am4charts.DateAxis());
+    dateAxis.renderer.minGridDistance = 50;
+
+    var valueAxis = c_decessi_covid_decessi_20.yAxes.push(new am4charts.ValueAxis());
+
+    // Create series
+    var series = c_decessi_covid_decessi_20.series.push(new am4charts.LineSeries());
+    series.dataFields.valueY = "value";
+    series.dataFields.dateX = "date";
+    series.strokeWidth = 2;
+    series.minBulletDistance = 10;
+    series.tooltipText = "{valueY}";
+    series.tooltip.pointerOrientation = "vertical";
+    series.tooltip.background.cornerRadius = 20;
+    series.tooltip.background.fillOpacity = 1;
+    series.tooltip.label.padding(12, 12, 12, 12)
+    series.numberFormatter.numberFormat = "#'%'"
+
+    // Add scrollbar
+    c_decessi_covid_decessi_20.scrollbarX = new am4charts.XYChartScrollbar();
+    c_decessi_covid_decessi_20.scrollbarX.series.push(series);
+
+    // Add cursor
+    c_decessi_covid_decessi_20.cursor = new am4charts.XYCursor();
+    c_decessi_covid_decessi_20.cursor.xAxis = dateAxis;
+    c_decessi_covid_decessi_20.cursor.snapToSeries = series;
+    c_decessi_20_decessi_15_18.svgContainer.autoResize = false;
+}
 
 fillGraphs()
